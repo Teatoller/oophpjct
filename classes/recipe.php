cb114a712772dd0475b39fa3eb9ae898c7586869
Teatoller <steven.otieno1@gmail.com>
2019-11-13 09:22:16 +0300

    (setter mthd): Implement setter method
    - add setter to sanitize title input
    
    [Starts #003]

--- classes/recipe.php
+++ classes/recipe.php
@@ -12,15 +12,23 @@ class Recipe
     {
         return $this->title . " by " . $this->source;
     }
+
+    public function setTitle($title)
+    {
+        $this->title = ucwords($title);
+    }
+
+    public function getTitle()
+    {}
 }
 
 $recipe1 = new Recipe();
 $recipe1->source = "HAgama" . "\n";
-$recipe1->title = "My First Recipe";
+$recipe1->setTitle("my first recipe");
 
 $recipe2 = new Recipe();
 $recipe2->source = "Luwi" . "\n";
-$recipe2->title = "My Second Recipe";
+$recipe2->title = "my second recipe";
 
 echo $recipe1->displayRecipe();
 echo $recipe2->displayRecipe();
