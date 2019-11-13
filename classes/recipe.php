6182e2f5f8c50f0b8ce17a15cc93c3810fc5295b
Teatoller <steven.otieno1@gmail.com>
2019-11-13 11:33:09 +0300

    (ingredient method): Implement ingredient method
    - add method for ingredient
    
    [Starts #006]

--- classes/recipe.php
+++ classes/recipe.php
@@ -7,6 +7,33 @@ class Recipe
     public $yield;
     public $tag = array();
     public $source = "Treehouse";
+    private $measurements = array(
+        "tsp",
+        "tasp",
+        "cup",
+        "oz",
+        "lb",
+        "fl oz",
+        "pint",
+        "quart",
+        "gallon",
+    );
+
+    public function addIngredient($item, $amount = null, $measure = null)
+    {
+        if ($amount != null && !is_float($amount) && !is_int($amount)) {
+            exit("The amount must be a float: " . gettype($amount) . " $amount given");
+        };
+        if ($measure != null && !in_array(strtolower($measure), $this->measurements)) {
+            exit("Please enter a valid measurement: " . implode(", ", $this->measurements));
+        };
+
+        $this->ingredients[] = array(
+            "$item" => ucwords($item),
+            "$amount" => $amount,
+            "$measure" => strtolower($measure),
+        );
+    }
 
     public function displayRecipe()
     {
@@ -27,6 +54,7 @@ class Recipe
 $recipe1 = new Recipe();
 $recipe1->source = " HAgama" . "\n";
 $recipe1->setTitle(" my first recipe");
+$recipe1->addIngredient("egg", 2, "oz");
 
 $recipe2 = new Recipe();
 $recipe2->source = " Luwi" . "\n";
